e698ea8cbf9ef3743e552896fd3ca8963961408e
gracefideledushime <gngrace10@gmail.com>
2026-05-15 21:30:24 -0500

    Test both delete functions

--- A5_P1/main.cpp
+++ A5_P1/main.cpp
@@ -1,4 +1,5 @@
 #include "binomial.h"
+#include "customErrorClass.h"
 
 int main()
 {
@@ -14,10 +15,37 @@ int main()
     bh.insert(3);
     bh.insert(12);
 
-    cout << "Heap after insertions:" << endl;
-    bh.printHeap();
+    try
+    {
+        cout << "Heap after insertions:" << endl;
+        bh.printHeap();
 
-    cout << endl
-         << "Minimum key: " << bh.findMin()->key << endl;
+        cout << endl
+             << "Minimum key (initial): " << bh.findMin()->key << endl;
+
+        bh.deleteMin();
+        bh.deleteMin();
+        bh.deleteMin();
+
+        cout << "Heap after 3 min deletions:" << endl;
+        bh.printHeap();
+
+        cout << endl
+             << "Minimum key: " << bh.findMin()->key << endl;
+
+        cout << "Heap after decreaseKey deletions:" << endl;
+        bh.printHeap();
+
+        bh.deleteKey(24);
+        int min = bh.findMin()->key;
+        bh.deleteKey(min);
+        cout << endl
+             << "Minimum key: " << bh.findMin()->key << endl;
+    }
+    catch (MyException &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
 }
